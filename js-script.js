90d1cc792f4629d830e6945a074420f0a157306c
Machalo <150803687+Machalo@users.noreply.github.com>
2024-06-17 14:09:38 +0200

    Add files via upload
    Canvas that can be reseted, with decrese opacity evrytime the mouse hovers each pixel.

--- js-script.js
+++ js-script.js
@@ -1,15 +1,49 @@
+const btn = document.querySelector("button");
+
 const container = document.querySelector("#container");
 
-for (i=0; i<16 ; i++) {
-    const line = document.createElement("div");
-    line.setAttribute("class", `line${i}`);
-    container.appendChild(line)
-    for (j=0; j<16 ; j++) {
-        const div = document.createElement("div");
-        div.setAttribute("class", `div${j}`);
-        div.style.backgroundColor = "black";
-        div.textContent = "&nbsp";
-        line.appendChild(div);
+
+
+function resetCanvas(){
+    while (container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+
+    const text = document.getElementById("numPx").value;
+
+    if(!isNaN(text) && text>0 && text<100){
+    
+        for (i=0; i<text ; i++) {
+            const line = document.createElement("div");
+            line.setAttribute("class", "line");
+            container.appendChild(line)
+            for (j=0; j<text ; j++) {
+                const div = document.createElement("div");
+                div.setAttribute("class", "box");
+                div.style.backgroundColor = "black";
+                div.style.opacity = 1;
+                line.appendChild(div);
+            }
+        }
+        function color(box){
+            let startOpacity = box.style.opacity;
+            let newOpacity = startOpacity -0.1;
+            box.style.opacity = newOpacity;
+        }
+        const boxes = document.querySelectorAll(".box");
+        boxes.forEach((box) => {
+            box.addEventListener("mouseover",() => {
+                color(box);
+            })
+        })
+    }
+    else {
+        alert("Enter a number between 0 and 100 only!");
     }
 }
+
+btn.addEventListener("click", () => resetCanvas())
+
+
+
 
